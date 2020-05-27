c3053e052d812812a412094c7281c803e6846608
Tamie <tmap469@gmail.com>
2020-05-27 01:16:18 -0400

    Functionality - Initial fully working version complete
    The game is now fully playable.  Players give names and
    scores are tallied and displayed, it determines when a
    player has won, and gives an option for a new game.  Not
    in a polished state yet, though, could use some upgrades
    and changes, but at least it works!

--- src/App.js
+++ src/App.js
@@ -39,15 +39,17 @@ class GameBoardSquare extends Component {
 		];
 
 		/*TODO: Find more elegant way of doing this... possibly default invisible but made visible? */
-		if ("horizLine" in this.props.square) { 
+		if (this.props.square["horizLine"]) { 
 			squareChildren.push(<div key={4} className="horizLine" />)}
-		if ("vertLine" in this.props.square) {
+		if (this.props.square["vertLine"]){
 			squareChildren.push(<div key={5} className="vertLine" />)}
-		if ("greyedHorizLine" in this.props.square) {
+		if (this.props.square["greyedHorizLine"]) {
 			squareChildren.push(<div key={6} className="greyedHorizLine" />)}
-		if ("greyedVertLine" in this.props.square) {
+		if (this.props.square["greyedVertLine"]) {
 			squareChildren.push(<div key={7} className="greyedVertLine" />)}
-		
+		if ("takenBy" in this.props.square) {
+			squareChildren.push(<div key={8} className="boxLabel" align="center"> {this.props.square.takenBy} </div>);}
+
 		return (
 			<div className={className} >
 				{squareChildren}
@@ -164,13 +166,16 @@ class GameBoard extends Component {
 			"row": Math.min(row, this.state.selectedCoord.row),
 			"column": Math.min(column, this.state.selectedCoord.column) 
 		};
-		const newSquare = {...this.state.squares[moveCoords.row][moveCoords.column]};	
-		if (isHorizontalLine(this.state.selectedCoord, {"row": row, "column": column})) {
-			newSquare["horizLine"] = true;
+		const isHorizontal = isHorizontalLine(this.state.selectedCoord, {"row": row, "column": column});
+		const newSquareVals = {};
+		if (isHorizontal) {
+			newSquareVals["horizLine"] = true;
 		} else {
-			newSquare["vertLine"] = true;
+			newSquareVals["vertLine"] = true;
 		}
-		this.updateBoardSquare(newSquare, moveCoords.row, moveCoords.column);	
+		const numberBoxes = this.numberBoxesMadeByMove(moveCoords.row, moveCoords.column, isHorizontal);
+		this.updateBoardSquare(newSquareVals, moveCoords.row, moveCoords.column);	
+		this.props.onGameMove(numberBoxes);
 		return;
 	}
 
@@ -179,13 +184,13 @@ class GameBoard extends Component {
 			"row": Math.min(row, this.state.selectedCoord.row),
 			"column": Math.min(column, this.state.selectedCoord.column)
 		};
-		const newSquare = {...this.state.squares[possibleMoveCoords.row][possibleMoveCoords.column]};
+		const newSquareVals = {};
 		if (isHorizontalLine(this.state.selectedCoord, {"row": row, "column": column})) {
-			newSquare["greyedHorizLine"] = true;
+			newSquareVals["greyedHorizLine"] = true;
 		} else {
-			newSquare["greyedVertLine"] = true;
+			newSquareVals["greyedVertLine"] = true;
 		}
-		this.updateBoardSquare(newSquare, possibleMoveCoords.row, possibleMoveCoords.column);
+		this.updateBoardSquare(newSquareVals, possibleMoveCoords.row, possibleMoveCoords.column);
 		return;
 	}
 
@@ -195,13 +200,13 @@ class GameBoard extends Component {
 			"row": Math.min(row, this.state.selectedCoord.row),
 			"column": Math.min(column, this.state.selectedCoord.column)
 		};
-		const newSquare = {...this.state.squares[possibleMoveCoords.row][possibleMoveCoords.column]};
+		const newSquareVals = {};
 		if (isHorizontalLine(this.state.selectedCoord, {"row": row, "column": column})) {
-			delete newSquare["greyedHorizLine"];
+			newSquareVals["greyedHorizLine"] = false;
 		} else {
-			delete newSquare["greyedVertLine"];
+			newSquareVals["greyedVertLine"] = false;
 		}
-		this.updateBoardSquare(newSquare, possibleMoveCoords.row, possibleMoveCoords.column);
+		this.updateBoardSquare(newSquareVals, possibleMoveCoords.row, possibleMoveCoords.column);
 		return;
 	}
 
@@ -215,33 +220,176 @@ class GameBoard extends Component {
 		return;
 	}
 
+	numberBoxesMadeByMove(moveRow, moveColumn, isHorizontal) {
+		let numberBoxesMade = 0;
+		if (isHorizontal) {
+			// Completes box above?
+			if (((moveRow - 1) >= 0) && 
+					("vertLine" in this.state.squares[moveRow-1][moveColumn]) &&
+					("horizLine" in this.state.squares[moveRow-1][moveColumn]) &&
+					("vertLine" in this.state.squares[moveRow-1][moveColumn+1])) {
+				numberBoxesMade++;
+				this.labelBox(moveRow-1, moveColumn);
+			}
+			// Completes box below?
+			if (((moveRow + 1) < this.props.boardHeight) &&
+					("vertLine" in this.state.squares[moveRow][moveColumn]) &&
+					("horizLine" in this.state.squares[moveRow+1][moveColumn]) &&
+					("vertLine" in this.state.squares[moveRow][moveColumn+1])) {
+				numberBoxesMade++;
+				this.labelBox(moveRow, moveColumn);
+			}
+		} else { // Is vertical
+			// Completes box to the right?
+			if (((moveColumn + 1) < this.props.boardWidth) &&
+					("horizLine" in this.state.squares[moveRow][moveColumn]) &&
+					("vertLine" in this.state.squares[moveRow][moveColumn+1]) &&
+					("horizLine" in this.state.squares[moveRow+1][moveColumn])) {
+				numberBoxesMade++;
+				this.labelBox(moveRow, moveColumn);
+			}
+			// Completes box to the left
+			if (((moveColumn - 1) >= 0) &&
+					("horizLine" in this.state.squares[moveRow][moveColumn-1]) &&
+					("vertLine" in this.state.squares[moveRow][moveColumn-1]) &&
+					("horizLine" in this.state.squares[moveRow+1][moveColumn-1])) {
+				numberBoxesMade++;
+				this.labelBox(moveRow, moveColumn-1);
+			}
+		}
+		return numberBoxesMade;
+	}		
 
-	updateBoardSquare(newSquare, row, column){
+	labelBox(boxRow, boxColumn) {
+		this.updateBoardSquare({takenBy: this.props.currentPlayerInitials()}, boxRow, boxColumn);
+		return;
+	}
+
+	updateBoardSquare(newSquareVals, row, column) {
 		// Map through rows and columns to find desired element location to mutate
-		this.setState({squares: 
-			this.state.squares.map((squareRow, j) => {
+		this.setState((state, props) => ({squares: 
+			state.squares.map((squareRow, j) => {
 				return squareRow.map((square, i) => {
 					if(i == column && j == row) {
-						return newSquare;
+						return Object.assign({...square}, newSquareVals);
 					} else {
 						return square;
 					}
 				})
 			})
-		});
+		}));
 	}
 	
 }
 
-class App extends Component {
+class Game extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			score1: 0,
+			score2: 0,
+			playerName1: null,
+			playerName2: null,
+			boardWidth: 0,
+			boardHeight: 0,
+			firstPlayerGoes: true,
+			setupComplete: false
+		};
+	}
+
+	componentDidMount() {
+		//TODO: Validate inputs, at least.
+		const playerName1 = prompt("Player 1, please enter your name");
+		const playerName2 = prompt("Player 2, please enter your name");
+		const boardSize = parseInt(prompt("What size board would you like?"), 10);
+		this.setState({
+			playerName1: playerName1,
+			playerName2: playerName2,
+			boardWidth: boardSize,
+			boardHeight: boardSize,
+			setupComplete: true
+		});
+		return;
+	}
+
+	componentWillUnmount() {
+		// Currently empty, may not be needed.
+	}
+
+	componentDidUpdate() {
+		//TODO: Find a way to do this that updates all UI before ending game
+		if ((this.state.score1 + this.state.score2) == ((this.state.boardHeight - 1) * (this.state.boardWidth - 1))) {
+			this.determineWinner();
+		}
+		return;
+	}
+
+	onGameMove(numberBoxesCompleted) {
+		if (numberBoxesCompleted > 0) {
+			let whichScore = null;
+			// Do this with a ?: conditional statement?
+			if (this.state.firstPlayerGoes) {
+				whichScore = "score1";
+			} else {
+				whichScore = "score2";
+			}
+			// Player goes again after completing a box
+			this.setState((state, props) => (
+				{[whichScore]: state[whichScore] + numberBoxesCompleted}));
+		} else {
+			this.setState((state, props) => ({firstPlayerGoes: !state.firstPlayerGoes}));
+		}
+		return;
+	}
+
+	determineWinner() {
+		if (this.state.score1 == this.state.score2) {
+			alert("The Match was a tie!!");
+		} else if (this.state.score1 > this.state.score2) {
+			alert(this.state.playerName1 + " is the winner");
+		} else {
+			alert(this.state.playerName2 + " is the winner");
+		}
+		if (confirm("Would you like to play again?")) {
+			const boardSize = parseInt(prompt("What board size would you like?"), 10);
+			this.setState({boardWidth: boardSize, boardHeight: boardSize});
+		}
+	}
+
+	currentPlayerInitials() {
+		//TODO: handle multiple part names with up to three initials
+		// e.g. Hillary Rodham Clinton -> HRC
+		if (this.state.firstPlayerGoes) {
+			return this.state.playerName1.charAt(0);
+		} else {
+			return this.state.playerName2.charAt(0);
+		}
+	}
+
+	render() {
+		if (!this.state.setupComplete) {return null;}
+		// Setup is complete
+		// TODO: Should score be stored elsewhere?  Maybe state changes cause unnecessary GameBoard re-renderings?  Look into it.
+		return (
+ 			<div>
+				<h1>{this.state.playerName1}[{this.state.score1}] vs {this.state.playerName2}[{this.state.score2}]</h1>
+					<h2>Current player is {this.state.firstPlayerGoes? this.state.playerName1 : this.state.playerName2}</h2>
+				<GameBoard
+					boardWidth={this.state.boardWidth}
+					boardHeight={this.state.boardHeight}
+					onGameMove={this.onGameMove.bind(this)}
+					currentPlayerInitials={this.currentPlayerInitials.bind(this)}
+				/>
+			</div>
+		);
+	}
+}
+
+class App extends Component {	
 	render(){
 		return (
 			<div className="App">
-				<h1> Hello, World! </h1>
-				<GameBoard 
-					boardWidth={5}
-					boardHeight={5}
-				/>
+				<Game />
 			</div>
 		);
 	}
