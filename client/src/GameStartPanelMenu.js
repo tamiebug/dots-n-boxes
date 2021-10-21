tamiebug/dots-n-boxes

commit 0ced8e2ae06ba61e4582456d29f7fdd806b25c4e

ControlPanel - Decouple Start Menu
The change in architecture of the central data store has allowed
us to decouple the menu, through the usage of the appState state
variable, we can now trigger the appearance and disappearance
of the main menu without ControlPanel having to manage it; it
simply appears whenever we are in the "PRE_MATCH" appState, and
vanishes once we leave it, allowing all of the start menu's logic
to exist entirely within the GameSTartPanelMenu component.

client/src/GameStartPanelMenu.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import { GameMenu } from './GameMenu.js';
+import { appStates, useGameStore } from "./GameStore.js";
+import { DEFAULT_GAME_SETTINGS } from "./GameEngine.js";
+
 import { ChooseOnlineNameComponent } from "./StartMenu/ChooseOnlineNameComponent";
 import { AvailableGamesListComponent } from "./StartMenu/AvailableGamesListComponent";
 import { GameLobbyComponent } from "./StartMenu/GameLobbyComponent";
@@ -10,7 +14,28 @@ import { BoardSizeComponent } from "./StartMenu/BoardSizeComponent";
 import { ChoosePlayerNameComponent } from "./StartMenu/ChoosePlayerNameComponent";
 import { AIDifficultyComponent } from "./StartMenu/AIDifficultyComponent";
 
-export function GameStartPanelMenu({ previousSettings, setGameSettingsAndKillMenu }) {
+export function GameStartPanelMenu() {
+  const [ gameState, gameStateDispatch ] = useGameStore();
+  const [ previousSettings, setPreviousSettings ] = useState( DEFAULT_GAME_SETTINGS );
+
+  useEffect(() => {
+    let previousGameSettings = {};
+    if ( gameState.appSettings.savePreviousMatchSettings ) {
+      const previousGameSettingsString = window.localStorage.getItem('Previous Game Settings');
+      previousGameSettings = JSON.parse(previousGameSettingsString);
+    }
+    setPreviousSettings({ ...DEFAULT_GAME_SETTINGS, ...previousGameSettings });
+  }, [ ]);
+
+  function setGameSettingsAndKillMenu( settings ) {
+    setPreviousSettings({ ...settings });
+    if ( gameState.appSettings.savePreviousMatchSettings == true ) {
+      const stringifiedSettings = JSON.stringify(settings);
+      window.localStorage.setItem('Previous Game Settings', stringifiedSettings);
+    }
+    gameStateDispatch({ type: 'setUpGame', settings });
+  }
+
   const menuItems = {
     "Home Page": ({ linkTo, formData }) => <HomePageComponent { ...{ linkTo, formData, setGameSettingsAndKillMenu }} />,
     "Choose Opponent Type": ({ linkTo, formData, setFormData }) => <ChooseOpponentTypeComponent { ...{ linkTo, formData, setFormData }} />,
@@ -23,5 +48,5 @@ export function GameStartPanelMenu({ previousSettings, setGameSettingsAndKillMen
     "AI Difficulty": ({ linkTo, formData, setFormData }) => <AIDifficultyComponent { ...{ linkTo, formData, setFormData, previousSettings }} />,
   };
 
-  return <GameMenu name="GameMenu" startingItemName="Home Page" defaultFormSettings={ previousSettings } menuItems={ menuItems } />;
+  return gameState.appState == appStates.PRE_MATCH && <GameMenu name="GameMenu" startingItemName="Home Page" defaultFormSettings={ previousSettings } menuItems={ menuItems } />;
 }

client/src/ControlPanel.js
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { AppSettingsMenu } from "./AppSettingsMenu.js";
-import { useGameStore } from "./GameStore.js";
+import { appStates, useGameStore } from "./GameStore.js";
 import { GameStartPanelMenu } from "./GameStartPanelMenu.js";
 import { printObjectToJSON, MoveHistory } from "./utility.js";
 
-const DEFAULT_GAME_SETTINGS =  { boardHeight: 5, boardWidth: 5, playerNames: ["Player 1", "Player 2"], gameType: "local", cpuDifficulty: "random" };
-
 export function ControlPanel() {
   const [ gameState, gameStateDispatch ] = useGameStore();
-  const [ showStartMenu, setShowStartMenu ] = useState(true);
   const [ showAppSettingsMenu, setShowAppSettingsMenu ] = useState(false);
-  const [ previousSettings, setPreviousSettings ] = useState( DEFAULT_GAME_SETTINGS );
 
-  const { appSettings, players, currentPlayer, gameSettings, moveHistory, gameBoardState } = gameState;
-
-  useEffect(() => {
-    if ( appSettings.savePreviousMatchSettings ) {
-      const previousGameSettingsString = window.localStorage.getItem('Previous Game Settings');
-      const previousGameSettings = JSON.parse(previousGameSettingsString);
-      setPreviousSettings({ ...DEFAULT_GAME_SETTINGS, ...previousGameSettings });
-    } else {
-      setPreviousSettings(DEFAULT_GAME_SETTINGS);
-    }
-    gameStateDispatch({ type: 'setUpGame', settings: DEFAULT_GAME_SETTINGS });
-  }, [ ]);
+  const { appSettings, appState, players, currentPlayer, gameSettings, moveHistory, gameBoardState } = gameState;
 
   function onNewGame() {
-    setShowStartMenu(true);
+    gameStateDispatch({ type: 'returnToStartMenu' });
   }
 
   function loadGameStateFromJSON() {
@@ -50,24 +35,10 @@ export function ControlPanel() {
     inputElement.click();
   }
 
-  function setGameSettingsAndKillMenu(settings) {
-    setShowStartMenu(false);
-    setPreviousSettings({...settings});
-    if ( appSettings.savePreviousMatchSettings === true ) {
-      const stringifiedSettings = JSON.stringify(settings);
-      window.localStorage.setItem('Previous Game Settings', stringifiedSettings);
-    }
-    gameStateDispatch({ type: 'setUpGame', settings });
-  }
-
   return gameBoardState == null ? null : (
     <div className="col-sm-auto d-flex flex-column gameControlPanel jumbotron">
-      {showStartMenu == true && <GameStartPanelMenu
-        name="GameMenu"
-        previousSettings={ previousSettings }
-        setGameSettingsAndKillMenu={ s => setGameSettingsAndKillMenu(s) }
-      />}
-      {showAppSettingsMenu == true && <AppSettingsMenu
+      <GameStartPanelMenu name="GameMenu"/>
+      {showAppSettingsMenu == true && appState !== appStates.PRE_MATCH && <AppSettingsMenu
         name="AppSettingsMenu"
         setAppSettingsAndKillMenu= { appSettings => { setShowAppSettingsMenu(false); gameStateDispatch({ type: 'changeAppSettings', appSettings }); }}
       />}
@@ -79,7 +50,7 @@ export function ControlPanel() {
           <h2 className="who-goes">{ players[currentPlayer].name } goes</h2>
           <div className="row">
             <PanelButton
-              onMouseClick={() => setShowAppSettingsMenu(true)}
+              onMouseClick={() => { if ( appState !== appStates.PRE_MATCH ) setShowAppSettingsMenu(true); }}
               bootstrapType="secondary"
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">

client/src/GameEngine.js
@@ -7,6 +7,7 @@ import { printObjectToJSON } from "./utility";
 //export const useGameStore = () => useStore( gameStore );
 
 export const DEFAULT_APP_SETTINGS = { debugMode: false, savePreviousMatchSettings: true };
+export const DEFAULT_GAME_SETTINGS =  { boardHeight: 5, boardWidth: 5, playerNames: ["Player 1", "Player 2"], gameType: "local", cpuDifficulty: "random" };
 
 const changeHandlers = new Map(Object.entries({
   'moveHistory': moveHistoryChangeHandler,
@@ -17,6 +18,8 @@ export function GameEngine( store ) {
   store.subscribe( this.onStateBroadcast.bind( this ) );
   registerOnlineMoveCallback( move => store.dispatch({ type: 'onlineMoveAttempt', move }) );
   loadAppSettingsFromBrowser( store.dispatch );
+  applyDefaultGameSettings( store.dispatch );
+  store.dispatch({ type: 'returnToStartMenu' });
 }
 
 GameEngine.prototype.onStateBroadcast = function onStateBroadcast( state, oldState ) {
@@ -90,4 +93,8 @@ function loadAppSettingsFromBrowser( dispatch ) {
   if (localSettings) {
     dispatch({ type: 'changeAppSettings', appSettings: {...DEFAULT_APP_SETTINGS, ...localSettings }});
   }
+}
+
+function applyDefaultGameSettings( dispatch ) {
+  dispatch({ type: 'setUpGame', settings: DEFAULT_GAME_SETTINGS });
 }

client/src/GameStore.js
@@ -193,6 +193,9 @@ function gameStateReducer(state, action) {
     console.log("GAME DEACTIVATED");
     return {...state, appState: appStates.PAUSED };
 
+  case 'returnToStartMenu':
+    return { ...state, appState: appStates.PRE_MATCH };
+
   case 'showChains': {
     validateAction(action, [{ key: 'chains', 'instanceOf': Array}]);
     const boxTags = renderBoxChainsIntoTags(action.chains);

commit 561475e276b808c739b249b488fee992c6ca1bc4

GameBoard - Fix Error on Online Match Start
Fixes bug introduced in 0CED8E2A

For what seems to be no good reason at all [ this bug only happens
when starting an online match ], there will be two instances of
GameBoard when the state is first updated by setUpGame if the
reducer action is ultimately being triggered asynchronously by
a callback from Socket.js instead of being triggered ( also
asynchronously, technically ) by clicking a button on the
GameStartPanelMenu UI that calls it.

Even if gameStateReducer({ type: 'setUpGame, settings }) is called
with an identical settings object, it does not matter, we still get
this bug (if, for example, Socket.js is modified to always start
a CPU match and ignore all data received from server instead of setting
up an online match ).

After looking through many different possiblities for fixing it and
trying various things, this is the only one that works.  The "extra"
GameBoard vanishes immediately, too.  At least if it stuck around
there would be some sense to it.

This bug may be due to the changes with how state is initialized, and
the moving of some things out of useEffect statements and into
GameEngine.js, but a quick inspection of the code still leaves me
completely puzzled as to why this bug would occur.  Hopefully, in the
future, this commit can be reverted and the code will function without
this fix; poorly understood fixes are bad fixes, but of all possible
poorly understood fixes, this one is the least likely to cause
problems.

client/src/GameStartPanelMenu.js
@@ -33,6 +33,19 @@ export function GameStartPanelMenu() {
       const stringifiedSettings = JSON.stringify(settings);
       window.localStorage.setItem('Previous Game Settings', stringifiedSettings);
     }
+    /* The Following is a bugfix for bug introduced in commit 0CED8E2A
+     *
+     * For some reason, whenever this function's call stack originates from a callback in Socket.js, after this commit above
+     * the old version of the GameBoard coexists with the new version for the first update and first update only, causing
+     * errors, but only at setUpGame match initialization and at no other time.  By doing an extra setUpGame action, it seems 
+     * to prevent the error.  I have absolutely no clue why this is the case; setUpgame just does the trick and is able
+     * to get React to properly handle this function being called by a callback registered to Socket.js, specifically after the
+     * changes in the above commit which don't even change any of the seemingly relevant code.
+     * 
+     * In case that code changed in 0CED8E2A is further modified, it is worth going back here to see if this fix is stil needed.
+     * Applications are supposed to make sense, and this bug DOES have a yet-to-be-discovered reason for existing, whatever it may be.
+     */
+    gameStateDispatch({ type: 'setUpGame', settings: DEFAULT_GAME_SETTINGS }); 
     gameStateDispatch({ type: 'setUpGame', settings });
   }
 